506141e4b47b53ce9273d60d322ded719b486a51
Arthur Ace (XQ.AN) <arthur4it@gmail.com>
2016-09-20 01:02:08 +0800

    simplify info

--- app/controllers/transaction_controller.rb
+++ app/controllers/transaction_controller.rb
@@ -1,19 +1,18 @@
 class TransactionController < ApplicationController
   def initialize
     @listener = HouseListener.new
-    @count = 0
   end
 
   def home
     @listener.compute_transactions
     all= Transaction.all
-    info ="#{all.length} items ------\\n\n"
+    info ="#{all.length} items </br>"
     render :text =>info+show(all)
   end
 
   private 
   def show(txs)
-    txs.map{|i| [i.trx_date,i.district,i.total_area, i.rcount, i.resident_area].join("  \t\t  ")}.join("  \n  --  \\n ||||||||||||||||||| ")
+    txs.map{|i| [i.trx_date,i.district,i.total_area, i.rcount, i.resident_area].join("  \t\t  ")}.join(" </br>")
   end
 
 end
